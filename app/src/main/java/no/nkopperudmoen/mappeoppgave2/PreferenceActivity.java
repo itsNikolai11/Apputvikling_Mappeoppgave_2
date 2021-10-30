f888f0ab7668613a850c901bc7b3cd272ae34a35
Nikolai Kopperudmoen <nkopperudmoen@gmail.com>
2021-10-30 12:53:19 +0200

    Lagt til preferanse for tidspunkt når sms skal sendes. Endret PeriodiskService til å kjøre på dette tidspunktet.

--- app/src/main/java/no/nkopperudmoen/mappeoppgave2/PreferenceActivity.java
+++ app/src/main/java/no/nkopperudmoen/mappeoppgave2/PreferenceActivity.java
@@ -1,9 +1,19 @@
 package no.nkopperudmoen.mappeoppgave2;
 
+import android.annotation.SuppressLint;
+import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+
+import java.util.Calendar;
 
 import no.nkopperudmoen.mappeoppgave2.Fragments.PreferenceFragment;
 
@@ -12,9 +22,30 @@ public class PreferenceActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+        PreferenceFragment preferenceFragment = new PreferenceFragment();
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.preference_view, new PreferenceFragment())
+                .replace(R.id.preference_view, preferenceFragment)
                 .commit();
+        lastTidsvelger();
+    }
+
+    public void lastTidsvelger() {
+        Button btn = findViewById(R.id.editSmsTime);
+        Calendar cal = Calendar.getInstance();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        cal.setTimeInMillis(System.currentTimeMillis());
+        cal.set(Calendar.MINUTE, prefs.getInt(getString(R.string.smsAlertMin), 0));
+        cal.set(Calendar.HOUR, prefs.getInt(getString(R.string.smsAlertHour), 0));
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        int min = cal.get(Calendar.MINUTE);
+        btn.setOnClickListener(listener -> {
+            TimePickerDialog timeDialog = new TimePickerDialog(this, (timePicker, hour1, minute) -> {
+                prefs.edit().putInt(getString(R.string.smsAlertMin), minute).apply();
+                prefs.edit().putInt(getString(R.string.smsAlertHour), hour1).apply();
+            }, hour, min, true);
+
+            timeDialog.show();
+        });
     }
 }
--- app/src/main/java/no/nkopperudmoen/mappeoppgave2/Services/PeriodiskService.java
+++ app/src/main/java/no/nkopperudmoen/mappeoppgave2/Services/PeriodiskService.java
@@ -5,11 +5,15 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
+import androidx.preference.PreferenceManager;
 
 import java.util.Calendar;
+
+import no.nkopperudmoen.mappeoppgave2.R;
 
 public class PeriodiskService extends Service {
     @Nullable
@@ -21,10 +25,15 @@ public class PeriodiskService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Calendar cal = Calendar.getInstance();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        cal.setTimeInMillis(System.currentTimeMillis());
+        cal.set(Calendar.MINUTE, prefs.getInt(getString(R.string.smsAlertMin), 0));
+        cal.set(Calendar.HOUR, prefs.getInt(getString(R.string.smsAlertHour), 0));
         Intent i = new Intent(this, OrdreNotifyService.class);
         PendingIntent pIntent = PendingIntent.getService(this, 0, i, 0);
         AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), (60 * 1000) * 30, pIntent);
+        alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pIntent);
+        //alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), (60 * 1000) * 30, pIntent);
         return super.onStartCommand(intent, flags, startId);
     }
 }
